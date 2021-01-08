13d4e3d599299f69de752dad026701546de502da
Dineshkarthik <dineshkarthik.r@gmail.com>
2021-01-08 19:14:17 +0100

    refactor: bump-up version

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,6 @@
 """Init namespace"""
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 __license__ = "MIT License"
 __copyright__ = (
     "Copyright (C) 2019 Dineshkarthik <https://github.com/Dineshkarthik>"
