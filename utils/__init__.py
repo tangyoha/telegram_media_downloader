0226c1908d698bfa77fb2a234a01ed590f9f2bfc
Dineshkarthik <dineshkarthik.r@gmail.com>
2022-02-10 16:53:27 +0100

    build: bump-up telegram-media-downloader version from 1.2.1 to 1.3.0

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,6 @@
 """Init namespace"""
 
-__version__ = "1.2.1"
+__version__ = "1.3.0"
 __license__ = "MIT License"
 __copyright__ = (
     "Copyright (C) 2019 Dineshkarthik <https://github.com/Dineshkarthik>"
