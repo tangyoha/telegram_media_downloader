93986b14c1578762b383366ba5452936b5bb3c31
Dineshkarthik <dineshkarthik.r@gmail.com>
2022-02-14 13:33:31 +0100

    enh: use rich Console to print meta information

--- utils/meta.py
+++ utils/meta.py
@@ -1,7 +1,9 @@
 """Utility module to manage meta info."""
 import platform
 
-from . import __version__, __copyright__, __license__
+from rich.console import Console
+
+from . import __copyright__, __license__, __version__
 
 APP_VERSION = f"Telegram Media Downloader {__version__}"
 DEVICE_MODEL = (
@@ -13,7 +15,11 @@ LANG_CODE = "en"
 
 def print_meta(logger):
     """Prints meta-data of the downloader script."""
-    print(f"Telegram Media Downloader v{__version__}, {__copyright__}")
-    print(f"Licensed under the terms of the {__license__}", end="\n\n")
+    console = Console()
+    # pylint: disable = C0301
+    console.log(
+        f"[bold]Telegram Media Downloader v{__version__}[/bold],\n[i]{__copyright__}[/i]"
+    )
+    console.log(f"Licensed under the terms of the {__license__}", end="\n\n")
     logger.info(f"Device: {DEVICE_MODEL} - {APP_VERSION}")
     logger.info(f"System: {SYSTEM_VERSION} ({LANG_CODE.upper()})")
