0c5a3bc2ffb559c6434de1105a29852ee4222cd0
tangyoha <tangyoha@outlook.com>
2023-02-14 13:51:43 +0800

    fix:update copyright

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,4 +2,4 @@
 
 __version__ = "2.1.2"
 __license__ = "MIT License"
-__copyright__ = "Copyright (C) 2022 tangyoha <https://github.com/tangyoha>"
+__copyright__ = "Copyright (C) 2023 tangyoha <https://github.com/tangyoha>"
